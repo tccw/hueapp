03e75bd724c646e57a8c80c1b5be5e3e7f55a27d
tccw <thomas.cowan@gmail.com>
2020-07-08 22:13:58 -0700

    Attempted to use dotenv and broke something. Getting 502 errors now.

--- webapp/app/app.py
+++ webapp/app/app.py
@@ -2,11 +2,19 @@
 
 from datetime import datetime
 from flask import Flask, render_template, request
+import os
 import sys
+#from dotenv import load_dotenv, find_dotenv
+from scene_scripts import auroraBorealis
 
-buswatch = "BUSWATCH-b72f6d14-69f1-408c-b2a1-c25c50bdd1d5"
-aurora = "AURORA-fd2b5e7a-7c1a-4feb-a1e5-c45adb7b1723"
+#load_dotenv(find_dotenv())
+
+#buswatch = os.environ.get("BUSWATCH_CODE")
+#aurora = os.environ.get("AURORA_CODE")
+buswatch = ""
+aurora = "AURORA-ca51142d-ce65-4c08-96d3-90cc5f7d12d0"
 empty_response = ('', 204)
+OK_response = ('', 200)
 app = Flask(__name__)
 
 
@@ -19,33 +27,24 @@ def index():
 @app.route("/ifttt-trigger", methods=['GET', 'POST'])
 def start_bus_watch():
     if request.content_length is None:
-        return empty_response
+        return ('Lenght is NONE', 204)
     elif request.content_length < 1e4:
         message = request.get_data(as_text=True)
 
     if message == buswatch:
-        # import and immediately run busIndicator.py
-        console_logger(message.lower())
         try:
             from scene_scripts import busIndicator
+            aurora
         except Exception as e:
             return str(e) + sys.version + "Path to python: " + sys.executable 
         return "buswatch"
     elif message == aurora:
-        # import and immediately run auroraBorealis.py
-        console_logger(message.lower())
         try:
-            from scene_scripts import auroraBorealis
-            return "ran and terminated"
-            #exec(open("./scene_scripts/auroraBorealis.py").read())
+            auroraBorealis.aurora(20)
+            return OK_response 
         except Exception as e:
             return str(e) + sys.version + "Path to python: " + str(sys.path)
-        return "aurora" 
-    return empty_response
-
-def console_logger(module):
-    print("Log {}: running {}".format(datetime.now(), module))
-
+    return ('No body in POST', 204)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8080)
--- webapp/app/scene_scripts/xmasScene.py
+++ webapp/app/scene_scripts/xmasScene.py
@@ -3,6 +3,7 @@ import numpy as np
 from phue import Bridge
 from math import floor
 from scene_scripts.helpers import load_file
+from datetime import datetime, timedelta
 
 # constants
 file = "/scene_scripts/data/data.txt"
@@ -20,7 +21,9 @@ b.set_light(lr_lamp[1], 'xy', [1., 0.2])
 x = np.pi
 step = 0.01
 
-while True:
-    x = x + step
-    bri_val = np.power(2, np.cos(x) + 1) * 50
-    b.set_light(lr_lamp, 'bri', floor(bri_val), transitiontime=1)
+def xmas(runtime_sec):
+    stop_time = datetime.now() + timedelta(seconds = runtime_sec)
+    while stop_time > datetime.now():
+        x = x + step
+        bri_val = np.power(2, np.cos(x) + 1) * 50
+        b.set_light(lr_lamp, 'bri', floor(bri_val), transitiontime=1)
